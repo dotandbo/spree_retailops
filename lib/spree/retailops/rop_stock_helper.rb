3bdcd6586ee7deac37648832a97e3d5505f3aa47
Stefan O'Rear <sorear@gudtech.com>
2014-04-27 02:38:41 +0000

    flesh out inventory

--- lib/spree/retailops/rop_stock_helper.rb
+++ lib/spree/retailops/rop_stock_helper.rb
@@ -2,9 +2,22 @@ module Spree
   module Retailops
     class RopStockHelper
       def apply_stock variant, stock_hash
-        p "apply stock"
-        p variant
-        p stock_hash
+        @locations ||= {}
+        current = {}
+
+        variant.stock_items.each do |si|
+          current[si.stock_location] = si.count_on_hand if si.count_on_hand != 0
+        end
+
+        stock_hash.each do |locname, qty|
+          locname = locname.to_s
+          location = @locations[locname] ||= StockLocation.find_or_create_by!(name: locname) { |l| l.admin_name = locname }
+
+          location.move variant, qty.to_i() - (current.delete(location) || 0)
+        end
+
+        # zero out unmentioned locations
+        current.each { |old_loc, old_qty| old_loc.move variant, -old_qty }
       end
     end
   end
