21b6325f1918935702f10f2eabd82bb4965d6bad
Stefan O'Rear <sorear@gudtech.com>
2014-05-03 10:09:35 +0000

    first crack at order exporter

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Spree::Core::Engine.routes.draw do
   namespace :retailops do
     post 'catalog', to: 'catalog#catalog_push'
     post 'inventory', to: 'inventory#inventory_push'
-    resources :orders, only: [:show, :index] do
-      post 'mark_exported', to: 'orders#export'
-      post 'completed', to: 'orders#completed'
-      post 'cancelled', to: 'orders#cancelled'
-      post 'refunded', to: 'orders#refunded'
-    end
+
+    get 'orders', to: 'orders#index'
+    post 'orders/mark_exported', to: 'orders#export'
+    # TODO: order settlement
   end
 end
