ed2980635e1aa1382fe7bad33bd83fa9b4ab001d
Stefan O'Rear <sorear@gudtech.com>
2014-05-04 07:10:32 +0000

    honor :retailops_import_by_default

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,8 @@
 module Spree
   Order.class_eval do
     validates :retailops_import, inclusion: { in: [ 'yes', 'no', 'done' ] }
+    before_validation on: :create do
+      self.retailops_import = Spree::Config[:retailops_import_by_default] ? 'yes' : 'no'
+    end
   end
 end
