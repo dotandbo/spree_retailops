f1f482d0eb1bbfee4d51a43e37018ee514ea33bb
Stefan O'Rear <sorear@gudtech.com>
2014-05-22 22:43:46 +0000

    for the benefit of future integrations, back orders should start out as "no"

--- db/migrate/20140501000119_add_retailops_import_to_spree_orders.rb
+++ db/migrate/20140501000119_add_retailops_import_to_spree_orders.rb
@@ -1,6 +1,6 @@
 class AddRetailopsImportToSpreeOrders < ActiveRecord::Migration
   def change
-    add_column :spree_orders, :retailops_import, :string, { default: 'yes' }
+    add_column :spree_orders, :retailops_import, :string, { default: 'no' }
     add_index  :spree_orders, [ :retailops_import ]
   end
 end
