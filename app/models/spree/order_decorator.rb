72616b0bccd61b20de4f38addf0c270b59d3b7bd
Stefan O'Rear <sorear@gudtech.com>
2014-05-31 08:07:41 +0000

    set retailops_import at checkout time, not create time

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,8 +1,8 @@
 module Spree
   Order.class_eval do
     validates :retailops_import, inclusion: { in: [ 'yes', 'no', 'done' ] }
-    before_validation on: :create do
-      self.retailops_import = Spree::Config[:retailops_import_by_default] ? 'yes' : 'no'
-    end
+  end
+  Order.state_machine.before_transition :to => :complete do |order|
+    order.retailops_import = Spree::Config[:retailops_import_by_default] ? 'yes' : 'no'
   end
 end
