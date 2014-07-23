951867732beee76a9303b324450bed7b6679ebde
Stefan O'Rear <sorear@gudtech.com>
2014-07-23 03:16:25 +0000

    support for setting backorder, avoid zero stock moves

--- lib/spree/retailops/rop_stock_helper.rb
+++ lib/spree/retailops/rop_stock_helper.rb
@@ -6,18 +6,28 @@ module Spree
         current = {}
 
         variant.stock_items.each do |si|
-          current[si.stock_location] = si.count_on_hand if si.count_on_hand != 0
+          @locations[si.stock_location.name] ||= si.stock_location
+          current[si.stock_location] = { stock_item: si, on_hand: si.count_on_hand, backorderable: si.backorderable }
         end
 
         stock_hash.each do |locname, qty|
           locname = locname.to_s
           location = @locations[locname] ||= StockLocation.find_or_create_by!(name: locname) { |l| l.admin_name = locname }
 
-          location.move variant, qty.to_i() - (current.delete(location) || 0)
+          old = current.delete(location) || { on_hand: 0, backorderable: false }
+          new = { on_hand: qty, backorderable: detailed["backorder"][locname] ? true : false }
+
+          stock_item = old[:stock_item] || location.stock_item_or_create(variant)
+
+          stock_item.stock_movements.create!(quantity: new[:on_hand] - old[:on_hand]) if new[:on_hand] != old[:on_hand]
+          stock_item.update!(backorderable: new[:backorderable]) if new[:backorderable] != old[:backorderable]
         end
 
         # zero out unmentioned locations
-        current.each { |old_loc, old_qty| old_loc.move variant, -old_qty }
+        # unsure if this should mention backorder
+        current.each do |old_loc, old|
+          old_loc.move variant, -old[:on_hand] if old[:on_hand] != 0
+        end
 
         if detailed && variant.respond_to?(:retailops_notify_inventory)
           variant.retailops_notify_inventory(detailed)
